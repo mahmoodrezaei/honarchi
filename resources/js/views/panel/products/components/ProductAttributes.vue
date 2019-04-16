1a21c1bc53ae170e0c68622a8178406e4621710f
Ehsan <ehsanmoodi@gmail.com>
2019-04-16 13:42:36 +0430

    WIP product attribute component almost done

--- resources/js/views/panel/products/components/ProductAttributes.vue
+++ resources/js/views/panel/products/components/ProductAttributes.vue
@@ -5,7 +5,7 @@
 
                 <div class="form-group  m-form__group row">
                         <label class="col-lg-12 col-md-12 col-sm-12 col-form-label">ویژگی‌‌های مورد نظر را انتخاب کنید:</label>
-                        <div data-repeater-list="" class="col-lg-12">
+                        <div data-repeater-list="" class="col-lg-12" v-for="(item, index) in items" :key="index">
                             <div class="form-group m-form__group row align-items-center">
                                 <div class="col-md-4">
                                     <div class="m-form__group m-form__group--inline">
@@ -13,23 +13,53 @@
                                             <label for="attribute-name"></label>
                                         </div>
                                         <div class="m-form__control">
-<!--                                            <input type="text" id="attribute-name" class="form-control m-input" placeholder="نام ویژگی">-->
-                                            <model-select placeholder="نام ویژگی" :options="attributeSelectBoxValues" @select="addToSelectedAttributes(item)"></model-select>
+                                            <model-select placeholder="نام ویژگی" :options="attributeSelectBoxValues" v-model="item.selectedAttribute"></model-select>
                                         </div>
                                     </div>
                                     <div class="d-md-none m--margin-bottom-10"></div>
                                 </div>
-                                <div class="col-md-4">
+                                <div class="col-md-4" v-if="item.selectedAttribute.type === 'text'">
                                     <div class="m-form__group m-form__group--inline">
                                         <div class="m-form__label">
-                                            <label for="colorValue" class="m-label m-label--single"></label>
+                                            <label for="text-value" class="m-label m-label--single"></label>
                                         </div>
                                         <div class="m-form__control">
-                                            <input type="text" value="" id="colorValue" class="form-control m-input" placeholder="رنگ مورد نظر">
+                                            <input type="text" value="" v-model="item.textValue" id="text-value" class="form-control m-input" placeholder="مقدار ویژگی">
                                         </div>
                                     </div>
                                     <div class="d-md-none m--margin-bottom-10"></div>
                                 </div>
+
+                                <div class="col-md-4" v-else-if="item.selectedAttribute.type === 'select' && item.selectedAttribute.configuration.type === 'choices'">
+                                    <div class="m-form__group m-form__group--inline">
+                                        <div class="m-form__label">
+                                            <label for="single-choice" class="m-label m-label--single"></label>
+                                        </div>
+                                        <div class="m-form__control">
+                                            <model-select placeholder="مقدار ویژگی"
+                                                          :options="singleChoiceSelectBoxValues(index)"
+                                                          v-model="item.singleChoice"></model-select>
+                                        </div>
+                                    </div>
+                                    <div class="d-md-none m--margin-bottom-10"></div>
+                                </div>
+
+                                <div class="col-md-4" v-else-if="item.selectedAttribute.type === 'select' && item.selectedAttribute.configuration.type === 'multiple'">
+                                    <div class="m-form__group m-form__group--inline">
+                                        <div class="m-form__label">
+                                            <label for="multiple-choice" class="m-label m-label--single"></label>
+                                        </div>
+                                        <div class="m-form__control">
+<!--                                            <input type="text" value="" id="multiple-choice" class="form-control m-input" placeholder="انتخاب چندتایی">-->
+                                            <multi-select placeholder="مقادیر ویژگی‌"
+                                                          :options="multipleChoiceSelectBoxValues(index)"
+                                                          :selected-options="tempMultipleChoices"
+                                                          @select="multipleChoicesOnSelect" @change="saveMultipleChoicesToSelectedAttribute(index)"></multi-select>
+                                        </div>
+                                    </div>
+                                    <div class="d-md-none m--margin-bottom-10"></div>
+                                </div>
+
                                 <div class="col-md-4">
                                     <div data-repeater-delete="" @click="removeField(index)"
                                          class="btn-sm btn btn-danger m-btn m-btn--icon m-btn--pill">
@@ -75,18 +105,25 @@
 </template>
 
 <script>
-    import { ModelSelect } from 'vue-search-select'
+    import { ModelSelect, MultiSelect } from 'vue-search-select'
 
     export default {
         name: "ProductAttributes",
 
-        components: { ModelSelect },
+        components: { ModelSelect, MultiSelect },
 
         data() {
             return {
                 attributes: [],
 
-                selectedAttribute: [],
+                items:[{
+                    selectedAttribute: {},
+                    textValue: '',
+                    singleChoice: {},
+                    multipleChoice: []
+                }],
+
+                tempMultipleChoices: []
             }
         },
 
@@ -104,17 +141,64 @@
                     .catch(errors => console.log(errors.response.data))
             },
 
-            addToSelectedAttributes(item) {
-                this.selectedAttribute.push(item)
+            multipleChoicesOnSelect(items, lastSelectItem) {
+                this.tempMultipleChoices = items;
+            },
+
+            saveMultipleChoicesToSelectedAttribute(index) {
+                console.log('data changed');
             },
 
             addField() {
-
+                this.items.push({
+                    selectedAttribute: {},
+                    textValue: '',
+                    singleChoice: {},
+                    multipleChoice: []
+                });
             },
 
-            removeField() {
+            removeField(index) {
+                if (this.items.length !== 1) {
+                    this.items.splice(index, 1);
+                }
+            },
 
-            }
+            singleChoiceSelectBoxValues(index) {
+                let choices = this.items[index].selectedAttribute.configuration.values;
+
+                let temp = [];
+
+                if (choices.length > 0) {
+                    choices.forEach(item => {
+                        temp.push({
+                            text: item.value,
+                            value: item.value,
+                        })
+                    })
+                }
+
+                return temp;
+            },
+
+            multipleChoiceSelectBoxValues(index) {
+                let multipleChoices =
+                    this.items[index].selectedAttribute.configuration ?
+                        this.items[index].selectedAttribute.configuration.values : [];
+
+                let temp = [];
+
+                if (multipleChoices.length > 0) {
+                    multipleChoices.forEach(item => {
+                        temp.push({
+                            text: item.value,
+                            value: item.value,
+                        })
+                    })
+                }
+
+                return temp;
+            },
         },
 
         computed: {
@@ -136,8 +220,6 @@
 
                 return attributes;
             },
-
-
 
             submitBtnLoaderClasses() {
 
