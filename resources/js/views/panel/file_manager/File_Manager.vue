f6c2d53c4ecc23e61f5b900c71fb3632e45909de
mohsen hasani <m0h314.hasani@gmail.com>
2019-04-22 12:03:05 +0430

    fix filemanager ui

--- resources/js/views/panel/file_manager/File_Manager.vue
+++ resources/js/views/panel/file_manager/File_Manager.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <iframe style="border: 0px;" height="500px" width="100%" src="filemanager?type=Images"></iframe>
+  <div class="m-grid__item m-grid__item--fluid m-wrapper">
+    <div class="m-content">
+      <div class="m-portlet m-portlet--mobile">
+        <div class="col col-lg">
+          <div class="m-portlet__head">
+            <div class="m-portlet__head-caption">
+              <div class="m-portlet__head-title">
+                <h3 class="m-portlet__head-text">مدیریت فایل ها</h3>
+              </div>
+            </div>
+          </div>
+          <iframe style="border: 0px;" height="500px" width="100%" src="filemanager?type=Images"></iframe>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
